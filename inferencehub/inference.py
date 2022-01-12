503d5aa4a6ea6163bd2a2bf039ec06ba64b46f6a
Jorge Machado <4804546+jomach@users.noreply.github.com>
2022-01-12 10:30:42 +0100

    wip

--- inferencehub/inference.py
+++ inferencehub/inference.py
@@ -5,23 +5,7 @@ import dnnlib
 import legacy
 
 
-class ModelWrapper(nn.Module):
-    def __init__(self, network_pkl):
-        super().__init__()
-        # network parameters
-        self.network_pkl = network_pkl
 
-        # model loading
-        with dnnlib.util.open_url(self.network_pkl) as f:
-            self.model = legacy.load_network_pkl(f)['G_ema'].to("cpu")
-
-    def forward(self, x: dict) -> torch.tensor:
-        z = x.get("z")
-        c = x.get("c")
-
-        out = self.model(z, c)
-
-        return out
 
 
 def preprocess_function(input_pre: dict) -> dict:
---
+++ inferencehub/model.py
@@ -0,0 +1,28 @@
+from typing import Dict
+import legacy
+import dnnlib
+import torch 
+import torch.nn as nn
+
+class ModelWrapper(nn.Module):
+    def __init__(self, network_pkl):
+        super().__init__()
+        # network parameters
+        self.network_pkl = network_pkl
+
+        # model loading
+        with dnnlib.util.open_url(self.network_pkl) as f:
+            self.model = legacy.load_network_pkl(f)['G_ema'].to("cpu")
+        self.add_module("model", self.model)
+
+    def forward(self, x: dict) -> torch.tensor:
+        z = x.get("z")
+        c = x.get("c")
+
+        out = self.model(z, c)
+
+        return out
+
+def get_model(weights_path: str = None, map_location="cpu",
+              model_initialization_parameters: Dict = None) -> torch.nn.Module:
+              return ModelWrapper(network_pkl=weights_path)
